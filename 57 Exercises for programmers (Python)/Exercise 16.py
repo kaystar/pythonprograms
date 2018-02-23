2ad2d1bfab9124f70ba16c8ffef0f8646f81407d
kaystar <k.a.wood91@gmail.com>
2018-02-23 21:10:07 +0000

    Moved Exercises into own directory

---
+++ 57 Exercises for programmers (Python)/Exercise 16.py
@@ -0,0 +1,26 @@
+legal_age = 16
+
+def yes():
+    print("Yes. You're of legal age to drive")
+
+def no():
+    print("Sorry you're not of legal age to drive")
+
+while True:
+    try:
+        age = int(input("What is your age?: "))
+        if age >= 16:
+            yes()
+        elif age < 0:
+            print("Please input a positive number")
+            continue
+        else:
+            no()
+    except ValueError:
+        print("Please input an integer")
+        continue
+
+
+
+#yes if age >= legal_age else no
+
